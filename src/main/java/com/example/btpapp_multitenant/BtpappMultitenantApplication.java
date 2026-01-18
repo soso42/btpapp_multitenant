7f2a1625d01e68563fc33988654f47281631b489
Soso Janashvili <sosojanashvili@gmail.com>
2026-01-19 00:42:50 +0400

    initial commit

---
+++ src/main/java/com/example/btpapp_multitenant/BtpappMultitenantApplication.java
@@ -0,0 +1,13 @@
+package com.example.btpapp_multitenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtpappMultitenantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtpappMultitenantApplication.class, args);
+    }
+
+}
